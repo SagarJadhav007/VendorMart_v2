ff841d10ec782e3b5c5177b3ee577837eca99d27
Sagar Jadhav <jadhavsagar1505@gmail.com>
2025-07-26 21:50:42 +0530

    Removed nested git and re-added Frontend

---
+++ Backend/src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String },
+  password: { type: String, required: true },
+  role: { type: String, enum: ["vendor", "wholesaler"], required: true },
+  address: { type: String },
+}, { timestamps: true });
+
+// Hash password 
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare passwords
+userSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("User", userSchema);
